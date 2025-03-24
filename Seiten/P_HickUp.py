b431b6ba7be1da6e3911cbe7958dee7218850fe9
Martin Wolf <martin_wolf@bat.com>
2025-03-24 10:53:34 +0100

    Yes

--- Seiten/P_HickUp.py
+++ Seiten/P_HickUp.py
@@ -9,8 +9,6 @@ from Data_Class.AzureStorage import upload_file_to_folder
 from Data_Class.st_int_to_textbox import Int_to_Textbox
 from Data_Class.MMSQL_connection import read_Table, save_Table_append
 from Data_Class.sql import SQL
-from Data_Class.eml_msg_to_pdf import process_uploaded_file
-from Data_Class.st_AgGridCheckBox import AG_Select_Grid
 import fitz  # PyMuPDF
 from PIL import Image
 import io
